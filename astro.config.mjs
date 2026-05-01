214901345ce3cf26892c747c2cbf593554395752
Gunit kumar <43469241+Fexaop@users.noreply.github.com>
2026-05-01 14:59:02 +0530

    fix: url

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,8 @@ import react from '@astrojs/react';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
+  site: "https://zyphravision.github.io",
+  base: "/ShowcaseSite/",
   integrations: [react()],
   vite: {
     plugins: [tailwindcss()],
